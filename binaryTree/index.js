c643f892e2e78866ba7cbcb1d9229e971ef586f8
hux <huxley.millard@gmail.com>
2025-06-06 19:28:19 +0200

    feat: more binary but like this is fun i need to understand what my AST wantssssss

--- binaryTree/index.js
+++ binaryTree/index.js
@@ -10,19 +10,70 @@ class BinaryTree {
     }
 
     printTree(){
-        console.log(this.head)
+        console.log(JSON.stringify(this.head))
     }
 
+
+    depthFirstTraversal(target){
+
+        let res = { found: false, depth: undefined}
+
+        const recursiveTraverse = (currentNode, depth, target) => {
+
+
+            if(currentNode && currentNode?.val == target) {
+                res.found = true
+                res.depth = depth
+                return depth
+            }
+
+            if(!currentNode?.left && !currentNode?.right) {
+                return depth
+            }
+
+            let currDepth = depth + 1
+
+
+            let left, right;
+            left = right = currDepth;
+    
+
+            if(currentNode?.left) {
+                left = recursiveTraverse(currentNode?.left, currDepth, target)
+            }
+
+            if(currentNode?.right) {
+                right = recursiveTraverse(currentNode?.right, currDepth, target)
+            }
+
+            return Math.max(left, right)
+
+        }
+
+
+        let deep = recursiveTraverse(this.head, 1, target)
+
+        console.log({deep, res, target})
+
+        return deep
+
+    }
+
+    searchFor(val){
+
+
+    }
 
 
     addNodes(toAdd){
 
         let vals = [...toAdd]
 
-    
-       while(vals.length) {
 
+
+       while(vals.length) {
             let nextNode = this.head
+
             let currentVal = vals.pop();
 
             while(true){
@@ -46,14 +97,8 @@ class BinaryTree {
                         nextNode = nextNode.left
                     }
                 }
+            }
         }
-
-        // do stuff 
-
-        // but ya need to break
-       }
-
-
     }
 }
 
@@ -63,5 +108,6 @@ class BinaryTree {
 
 const oakyboy = new BinaryTree({val : 7})
 oakyboy.addNodes([3,2,5,1,7,4,2])
+oakyboy.depthFirstTraversal(1)
 oakyboy.printTree()
 
